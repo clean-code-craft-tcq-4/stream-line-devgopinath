21e30c0f360f9a7872059e50fc42a571384aebbf
Gopinath <25692465+devgopinath@users.noreply.github.com>
2022-09-08 00:43:06 +0530

    added stdlib.h

--- sender/sender.c
+++ sender/sender.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "sender.h"
 
 int getBatteryTemperature(void)
--- sender/sender_main.c
+++ sender/sender_main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "sender.h"
 
 int main(void)
