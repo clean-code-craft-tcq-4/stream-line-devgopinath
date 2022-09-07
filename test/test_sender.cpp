c85ea2f6eafe14a8c64162d03179b482b98bfbed
Gopinath <25692465+devgopinath@users.noreply.github.com>
2022-09-08 00:34:21 +0530

    application

---
+++ test/test_sender.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "sender.h"
+
+TEST_CASE("Test")
+{
+    streamSenderInputs();
+    REQUIRE(0==0);
+}
