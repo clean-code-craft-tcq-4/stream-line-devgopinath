a04d07920ce4fbb783a3b7a3f203b9bace48bf1c
Gopinath <25692465+devgopinath@users.noreply.github.com>
2022-09-08 01:37:27 +0530

    corrected string read

--- sender/sender.c
+++ sender/sender.c
@@ -14,12 +14,17 @@ int getBatteryChargeRate(void)
     return (rand() % 100);
 }
 
+void logSenderMessages(const char * msg)
+{
+
+}
+
 void streamSenderInputs(void)
 {
     for (int iteration = 0; iteration < NUM_OF_BATTERY_READINGS; ++iteration)
     {
         sprintf(&senderLog[iteration][0], "temperature:%d,charge-rate:%d", getBatteryTemperature(), getBatteryChargeRate());
-        printf("%s", senderLog[iteration][0]);
+        printf("%s", &senderLog[iteration][0]);
         printf("\n");
     }
 }
